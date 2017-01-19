b1ccf0800c2da1110c473fea994adf4e6a18c773
Fernando Varani <fervarani@gmail.com>
2017-01-19 14:58:47 -0200

    Adding some specs

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,52 +1,40 @@
 module Api::V1
   class MessagesController < ApiController
 
-    # GET /v1/messages
+    # POST api/v1/add_to_redis
     def add_to_redis
-      # binding.pry
-      # time = Benchmark.measure do
       # Parse and validated message
       message = Message.new(message_params)
       message.sent_at = Time.now
-
-      # binding.pry
-
       unless message.valid?
         render status: 422, json: {
-            error: message.errors.messages
-          }.to_json
-          return
+          error: message.errors.messages
+        }.to_json
+        return
       end
-
       json_message = message.to_json
 
-      # Create first message for this chat/live, if its first message
+      # Create first message for this chat/live
       if $redis_msg.hgetall("chat-#{message.chat_id}").nil?
-          $redis_msg.hset("chat-#{message.chat_id}", "message_id", 1)
-          $redis_msg.hset("chat-#{message.chat_id}", "msg-1}", json_message)
+        $redis_msg.hset("chat-#{message.chat_id}", "message_id", 1)
+        $redis_msg.hset("chat-#{message.chat_id}", "msg-1}", json_message)
+      # Create new message on redis an increment message_id, key also used for totalization
+      else
+        message_id = $redis_msg.hincrby("chat-#{message.chat_id}", "message_id", 1)
+        $redis_msg.hset("chat-#{message.chat_id}", "msg-#{message_id}", json_message)
       end
-
-      # Create new message on redis an increment message_id, key also used to totalization
-      message_id = $redis_msg.hincrby("chat-#{message.chat_id}", "message_id", 1)
-      $redis_msg.hset("chat-#{message.chat_id}", "msg-#{message_id}", json_message)
-
-      # end
-      # render json: time
       render status: 200, json: {
-          message: message
-        }.to_json
+        message: message
+      }.to_json
 
       ######
       # $redis_msg.hgetall("chat-#{message.chat_id}")
       ######
 
-      # Get Live / Chat current redis data
-      # redis_messages = JSON.load($redis_msg.hgetall("chat-#{message.chat_id}"))
-      # redis_messages.merge message.to_json
-
+      # Get Live/Chat current redis data
 
       # Send notification to Pusher
-        # Or do nothing, because it could be sent with the client
+        # Or do nothing, because it could be sent within the client
 
     end
 
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -23,5 +23,28 @@ describe Api::V1::MessagesController, type: :controller do
   end
 
   context 'with wrong data' do
+    before do
+      post :add_to_redis, params: { message: { wrong: 'data' } }
+    end
+    it 'responds with a 422 status' do
+      expect(response.status).to eq 422
+    end
+  end
+
+  context 'with missing params' do
+    it 'respond with error for missing chat_id' do
+      post :add_to_redis, params: { message: @new_message.except('chat_id') }
+      expect(response.body).to eq "{\"error\":{\"chat_id\":[\"can't be blank\"]}}"
+    end
+
+    it 'respond with error for missing user_id' do
+      post :add_to_redis, params: { message: @new_message.except('user_id') }
+      expect(response.body).to eq "{\"error\":{\"user_id\":[\"can't be blank\"]}}"
+    end
+
+    it 'respond with error for missing body' do
+      post :add_to_redis, params: { message: @new_message.except('body') }
+      expect(response.body).to eq "{\"error\":{\"body\":[\"can't be blank\"]}}"
+    end
   end
 end
