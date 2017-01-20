9262007485a0ac2a3268a9d2db66ddc126fabe27
Fernando Varani <fervarani@gmail.com>
2017-01-20 13:49:57 -0200

    Mock pusher

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,9 +1,10 @@
 module Api::V1
   class MessagesController < ApiController
-
     # POST api/v1/add_to_redis
     def add_to_redis
       # Parse and validated message
+
+      # BEGIN ============================ Refactor to class method ==============================
       message = Message.new(message_params)
       message.sent_at = Time.now
       unless message.valid?
@@ -12,7 +13,7 @@ module Api::V1
         }.to_json
         return
       end
-      json_message = message.to_json
+      json_message = message.to_json(only: [:user_id, :chat_id, :body, :sent_at])
 
       # Create first message for this chat/live
       if $redis_msg.hgetall("chat-#{message.chat_id}").nil?
@@ -23,6 +24,11 @@ module Api::V1
         message_id = $redis_msg.hincrby("chat-#{message.chat_id}", "message_id", 1)
         $redis_msg.hset("chat-#{message.chat_id}", "msg-#{message_id}", json_message)
       end
+
+      # Post to Pusher
+      Pusher.trigger("chat-#{message.chat_id}", "new_message", json_message)
+      # ============================ Refactor to class method ============================== END
+
       render status: 200, json: {
         message: message
       }.to_json
--- app/controllers/concerns/pusher.rb
+++ app/controllers/concerns/pusher.rb
@@ -1,11 +1,9 @@
-# Mock from pusher-http-ruby gem (Just for simulation purposes)
-module Pusher
-  extend ActiveSupport::Concern
-
-  def trigger(channels, event_name, data, params = {})
+# MOCK PURPOSES ONLY
+class Pusher
+  def self.trigger(channels, event_name, data, params = {})
     puts "Posting to: #{channels}, event: #{event_name}"
     puts "data: #{data}"
     puts "params: #{params}"
+    return true
   end
-
 end
