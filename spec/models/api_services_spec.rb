77cf26e6ef81b1413bb2b77b06964481a8f3fbb3
Fernando Varani <fervarani@gmail.com>
2017-01-22 20:58:44 -0200

    Finish ApiServices specs

--- spec/models/api_services_spec.rb
+++ spec/models/api_services_spec.rb
@@ -9,20 +9,23 @@ RSpec.describe ApiServices, type: :model do
   before do
     MessagesHelper.mock_redis_messages(messages, $redis_msg)
     @redis_chat = ApiServices.find_redis_chat(1)
+    ApiServices.create_chat_messages(chat, @redis_chat)
   end
 
-  context 'based on redis temporary data' do
-    it 'find redis chat and returns a Hash with messages' do
-      expect(@redis_chat.class).to eq Hash
-      expect(@redis_chat.length).to eq messages.length + 1
-    end
+  it 'find redis chat and returns a Hash with messages' do
+    expect(@redis_chat.class).to eq Hash
+    expect(@redis_chat.length).to eq messages.length + 1
+  end
 
-    it 'Create messages on DB based on Hash of redis messages' do
-      ApiServices.create_chat_messages(chat, @redis_chat)
-      message = JSON.parse @redis_chat["msg-1"]
+  it 'create messages on DB based on Hash of redis messages' do
+    message = JSON.parse @redis_chat["msg-1"]
+    expect(@redis_chat.length - 1).to eq Message.count
+    expect(message["body"]).to eq Message.first.body
+  end
 
-      expect(@redis_chat.length - 1).to eq Message.count
-      expect(message["body"]).to eq Message.first.body
-    end
+  it 'create chat report with a valid chat' do
+    chat_report = ApiServices.create_chat_report(chat)
+    expect(chat_report.qty_messages).to eq chat.messages.length
+    expect(chat_report.qty_of_users).to eq chat.users.uniq.length
   end
 end
