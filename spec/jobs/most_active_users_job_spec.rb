307296f854f704f456e8518fa8d550ff291c36a8
Fernando Varani <fervarani@gmail.com>
2017-01-22 22:53:59 -0200

    Add job spec

--- spec/jobs/most_active_users_job_spec.rb
+++ spec/jobs/most_active_users_job_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
 
 RSpec.describe MostActiveUsersJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'perform and queue job' do
+    job =MostActiveUsersJob.perform_later
+    expect(job.job_id.present?).to eq true
+  end
 end
